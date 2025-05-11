4eef86b67a57677b16781674d8f8c238ee32d771
Marivellisse <gmarivellisse@gmail.com>
2025-05-11 00:43:50 -0400

    Add advanced solutions: reverse alphabet with case toggle, and remove char at index

---
+++ python-if_else_loops_functions/101-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+remove_char_at = __import__('101-remove_char_at').remove_char_at
+
+print(remove_char_at("Best School", 3))      # Bes School
+print(remove_char_at("Chicago", 2))          # Chcago
+print(remove_char_at("C is fun!", 0))        #  is fun!
+print(remove_char_at("School", 10))          # School
+print(remove_char_at("Python", -2))          # Python
