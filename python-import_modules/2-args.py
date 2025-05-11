a6c598341d1c074a2debf3b6ce9d7d49710862bd
Marivellisse <gmarivellisse@gmail.com>
2025-05-11 01:09:47 -0400

    Fix: ensure all scripts use '__main__' guard to prevent code from running on import

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
-    count = len(sys.argv) - 1
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
@@ -10,5 +12,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
 
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i, arg in enumerate(args, 1):
+        print("{}: {}".format(i, arg))
