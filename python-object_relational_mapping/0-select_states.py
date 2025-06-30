f22482c38c16cd802f941513e69f8d11e5cc8fbf
Marivellisse <gmarivellisse@gmail.com>
2025-06-29 21:58:00 -0400

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,31 @@
--- Lists all privileges of the MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get MySQL credentials and DB name from command line arguments
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db_name)
+
+    # Create a cursor object to execute queries
+    cur = db.cursor()
+
+    # Execute SQL query to fetch all states sorted by id
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all results and print them
+    for row in cur.fetchall():
+        print(row)
+
+    # Close cursor and connection
+    cur.close()
+    db.close()
 
