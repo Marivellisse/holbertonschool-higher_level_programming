53aa7d7d3f88d09ea04ccf672a8256a1efd09a0b
Marivellisse <gmarivellisse@gmail.com>
2025-06-29 21:42:36 -0400

    5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
--- Task 5: Create table unique_id with unique id
+-- Task 5: Create table unique_id with default id=1 and unique constraint
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
