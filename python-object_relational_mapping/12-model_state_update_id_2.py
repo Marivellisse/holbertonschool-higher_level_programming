22b6eec7e986241b3c1dae7240db22ade4083945
Marivellisse <gmarivellisse@gmail.com>
2025-06-29 22:18:10 -0400

    fix

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Updates the name of the State with id=2 to 'New Mexico'
+Updates the name of the State object with id = 2 to 'New Mexico'
 """
 
 import sys
@@ -9,15 +9,19 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    # Crear conexión con la base de datos
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
+
+    # Crear sesión
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Buscar estado con id = 2
     state = session.query(State).filter(State.id == 2).first()
     if state:
         state.name = "New Mexico"
