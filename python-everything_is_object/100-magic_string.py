6054467f23d5dc1bf81d0fcd6c30b2bc940472e2
Marivellisse <gmarivellisse@gmail.com>
2025-07-27 23:14:53 -0400

    adv

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def magic_string(_list=[]):
-    _list += ["BestSchool"]
-    return ", ".join(_list)
+def magic_string(_counter=[0]):
+    _counter[0] += 1
+    return ", ".join(["BestSchool"] * _counter[0])
 
